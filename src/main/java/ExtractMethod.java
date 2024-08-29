8089125b497dd39646b47e632cb9dcbc841c70d1
Paulo Roberto Farah <paulorfarah@gmail.com>
2024-08-29 16:10:07 -0300

    extract method finalizado

--- src/main/java/ExtractMethod.java
+++ src/main/java/ExtractMethod.java
@@ -1,4 +1,19 @@
 
 public class ExtractMethod {
+	void printOwing(String name) {
+		  printBanner();
+
+		  // Print details.
+		  System.out.println("name: " + name);
+		  System.out.println("amount: " + getOutstanding());
+		}
+	
+	private int getOutstanding() {
+		return 100;
+	}
+
+	private void printBanner() {
+		System.out.println("-- banner --");
+	}
 
 }
