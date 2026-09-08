0828a7c73bc20e04a3dab9a874290a15473628a9
Ruan Kestring <ruancarkestring@gmail.com>
2026-09-08 20:43:52 -0300

    realizado Extract Class - classe ExtractClass e classe Weapon com responsabilidades separadas

---
+++ src/main/java/refatoracoes/mover/ExtractClass/Weapon.java
@@ -0,0 +1,10 @@
+package refatoracoes.mover.ExtractClass;
+
+public class Weapon {
+    public int damage;
+    public int weaponStatus;
+
+    public int getDamage() {
+        return damage;
+    }
+}
