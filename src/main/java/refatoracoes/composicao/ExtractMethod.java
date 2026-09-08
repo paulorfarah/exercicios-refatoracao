c33d6ee5fcb351c328dae4dec81812ce3b5d1d03
Ruan Kestring <ruancarkestring@gmail.com>
2026-09-08 19:20:43 -0300

    realizado extract method

--- src/main/java/refatoracoes/composicao/ExtractMethod.java
+++ src/main/java/refatoracoes/composicao/ExtractMethod.java
@@ -3,11 +3,13 @@ package refatoracoes.composicao;
 public class ExtractMethod {
 	void printOwing(String name) {
 		  printBanner();
+		  printDetails(name);
+	}
 
-		  // Print details.
-		  System.out.println("name: " + name);
-		  System.out.println("amount: " + getOutstanding());
-		}
+	private void printDetails(String name) {
+		System.out.println("name: " + name);
+		System.out.println("amount: " + getOutstanding());
+	}
 	
 	private int getOutstanding() {
 		return 100;
