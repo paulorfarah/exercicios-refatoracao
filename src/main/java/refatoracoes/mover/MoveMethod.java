3400d80f4149e93b98c1a5c73ded3ea45b2a8ae6
Ruan Kestring <ruancarkestring@gmail.com>
2026-09-08 21:02:23 -0300

    realizado Move Method

--- src/main/java/refatoracoes/mover/MoveMethod.java
+++ src/main/java/refatoracoes/mover/MoveMethod.java
@@ -1,39 +1,21 @@
 package refatoracoes.mover;
 
-
-
 public class MoveMethod {
 	// ...
 
-	  public double calculateTotal() {
-	    double total = 0;
+	public double calculateTotal(User user) {
+		double total = 0;
 	    for (Product product : getProducts()) {
-	      total += product.quantity * product.price;
+			total += product.quantity * product.price;
 	    }
-	    total = applyRegionalDiscounts(total);
+	    total = user.applyRegionalDiscounts(total);
 	    return total;
-	  }
-
-	  private double applyRegionalDiscounts(double total) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	private Product[] getProducts() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	public double applyRegionalDiscounts(User user, double total) {
-	    double result = total;
-	    switch (user.getCountry()) {
-	      case "US": result *= 0.85; break;
-	      case "RU": result *= 0.75; break;
-	      case "CN": result *= 0.9; break;
-	      // ...
-	    }
-	    return result;
-	  }
 	
 	public class Product {
 
@@ -48,7 +30,16 @@ public class MoveMethod {
 		public String getCountry() {
 			return this.country;
 		}
+
+		public double applyRegionalDiscounts(double total) {
+			double result = total;
+			switch (this.getCountry()) {
+				case "US": result *= 0.85; break;
+				case "RU": result *= 0.75; break;
+				case "CN": result *= 0.9; break;
+				// ...
+			}
+			return result;
+		}
 	}
 }
-
-
