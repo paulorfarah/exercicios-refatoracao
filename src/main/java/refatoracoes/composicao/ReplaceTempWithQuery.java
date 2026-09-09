da40931bd0940f5a7a8c4f8691edb005c79cc988
RmlJZ <lrambo859@gmail.com>
2026-09-08 22:24:00 -0300

    insercao de metodo para substituir variavel temporaria por consulta

--- src/main/java/refatoracoes/composicao/ReplaceTempWithQuery.java
+++ src/main/java/refatoracoes/composicao/ReplaceTempWithQuery.java
@@ -1,13 +1,17 @@
 package refatoracoes.composicao;
 
 public class ReplaceTempWithQuery {
-	double calculateTotal(int quantity, double itemPrice) {
-		  double basePrice = quantity * itemPrice;
-		  if (basePrice > 1000) {
-		    return basePrice * 0.95;
-		  }
-		  else {
-		    return basePrice * 0.98;
-		  }
-		}
+
+    double calculateTotal(int quantity, double itemPrice) {
+
+        if (calculateBasePrice(quantity, itemPrice) > 1000) {
+            return calculateBasePrice(quantity, itemPrice) * 0.95;
+        } else {
+            return calculateBasePrice(quantity, itemPrice) * 0.98;
+        }
+    }
+
+    private double calculateBasePrice(int quantity, double itemPrice) {
+        return quantity * itemPrice;
+    }
 }
