78041d206cc787bcb00af449d15caee22fa59d9e
Ruan Kestring <ruancarkestring@gmail.com>
2026-09-08 21:39:24 -0300

    realizado Replace Conditional With Polymorphism

--- src/main/java/refatoracoes/simplificar/ReplaceConditionalWithPolymorphism.java
+++ src/main/java/refatoracoes/simplificar/ReplaceConditionalWithPolymorphism.java
@@ -5,33 +5,42 @@ public class ReplaceConditionalWithPolymorphism {
 	private static final int AFRICAN = 1;
 	private static final int NORWEGIAN_BLUE = 2;
 
+	abstract class Bird {
+		public abstract double getSpeed();
+
+		private double getBaseSpeed() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		private double getBaseSpeed(Object voltage) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		private int getLoadFactor() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+	}
+
+	public class European extends Bird {
+		public double getSpeed() { return super.getBaseSpeed(); }
+	}
+
+	public class African extends Bird {
+		private int numberOfCoconuts;
+
+		public double getSpeed() { return super.getBaseSpeed() - super.getLoadFactor() * numberOfCoconuts; }
+	}
+
+	public class Norwegian extends Bird {
+		private Object voltage;
+		private boolean isNailed;
+
+		public double getSpeed() { return (isNailed) ? 0 : super.getBaseSpeed(voltage); }
+	}
+
 	// ...
-	  double getSpeed(int type, int numberOfCoconuts, Object voltage, boolean isNailed) {
-	    switch (type) {
-	      case EUROPEAN:
-	        return getBaseSpeed();
-	      case AFRICAN:
-	        return getBaseSpeed() - getLoadFactor() * numberOfCoconuts;
-	      case NORWEGIAN_BLUE:
-			return (isNailed) ? 0 : getBaseSpeed(voltage);
-	    }
-	    throw new RuntimeException("Should be unreachable");
-	  }
-
-	private double getBaseSpeed() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	private double getBaseSpeed(Object voltage) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	private int getLoadFactor() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
+	double getSpeed(Bird bird) { return bird.getSpeed(); }
 }
