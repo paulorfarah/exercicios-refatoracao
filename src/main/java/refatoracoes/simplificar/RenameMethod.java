86c892743eb1ffaea0d8101ab7abfc35f5980520
Ruan Kestring <ruancarkestring@gmail.com>
2026-09-08 21:25:46 -0300

    realizado Rename Method

--- src/main/java/refatoracoes/simplificar/RenameMethod.java
+++ src/main/java/refatoracoes/simplificar/RenameMethod.java
@@ -4,10 +4,7 @@ public class RenameMethod {
 	  private String name = "name";
 	  private String lastName = "lastname";
 
-	  /**
-	   * Method returns customer's lastname.
-	   */
-	  String getlnm() {
+	  String getLastName() {
 	    return lastName;
 	  }
 }
