4d60f94d76bb96771917775da0e3b18506d85acb
ErickRibeiro66 <erick.ribeiroribeiro66@gmail.com>
2026-09-11 14:28:12 -0300

    Refactor renderBanner method for readability
    Extraindo variáveis de boolean para melhor visualização

--- src/main/java/refatoracoes/composicao/ExtractVariable.java
+++ src/main/java/refatoracoes/composicao/ExtractVariable.java
@@ -2,16 +2,16 @@ package refatoracoes.composicao;
 
 public class ExtractVariable {
 	void renderBanner(String platform, String browser, int resize) {
-		  if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-		       (browser.toUpperCase().indexOf("IE") > -1) &&
-		        wasInitialized() && resize > 0 )
-		  {
-		    // do something
-		  }
-		}
+        boolean Plataforma = platform.toUpperCase().indexOf("MAC") > -1;
+        boolean Browser = browser.toUpperCase().indexOf("IE") > -1;
+        boolean Resize = resize > 0;
+	if (Plataforma && Browser && wasInitialized() && resize > 0 ){
+                // do something
+        }
+    }
 
-	private boolean wasInitialized() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    private boolean wasInitialized() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 }
