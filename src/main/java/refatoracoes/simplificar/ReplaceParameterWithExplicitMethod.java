63b98f262cfbe9f7ab8cd2e26fd7f204ea0c3540
ErickRibeiro66 <erick.ribeiroribeiro66@gmail.com>
2026-09-11 15:56:12 -0300

    Refactor method to replace parameters with explicit methods
    Com isso a humanidade será feliz

--- src/main/java/refatoracoes/simplificar/ReplaceParameterWithExplicitMethod.java
+++ src/main/java/refatoracoes/simplificar/ReplaceParameterWithExplicitMethod.java
@@ -1,22 +1,10 @@
 package refatoracoes.simplificar;
 
-public class ReplaceParameterWithExplicitMethod {
-	void setValue(String name, int value, int height, int width) {
-		  if (name.equals("height")) {
-		    height = value;
-		    return;
-		  }
-		  if (name.equals("width")) {
-		    width = value;
-		    return;
-		  }
-		  Assert a = new Assert();
-		  a.shouldNeverReachHere();
-		}
-	
-	public class Assert {
-		public void shouldNeverReachHere() {
-			System.out.println("não deveria entrar aqui...");
-		}
+public class ReplaceParameterWithExplicitMethod {	
+	void setAltura(int valor){
+		heigth = valor;
+	}
+	void setLargura(int valor){
+		width = valor;
 	}
 }
