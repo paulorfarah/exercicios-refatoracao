420723aab37ee62d6e84dc52c60e906488129f0a
Mawan Girardi Dolejal <mawan.dolejal@gmail.com>
2026-09-15 20:27:04 -0300

    Refactor ExtractClass to use Weapon class
    Refactored the ExtractClass by creating a separate Weapon class to encapsulate weapon properties and methods.

--- src/main/java/refatoracoes/mover/ExtractClass.java
+++ src/main/java/refatoracoes/mover/ExtractClass.java
@@ -1,14 +1,41 @@
+// Weapon
+package refatoracoes.mover;
+
+public class Weapon {
+
+    private int damage;
+    private int weaponStatus;
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getWeaponStatus() {
+        return weaponStatus;
+    }
+
+    public void setWeaponStatus(int weaponStatus) {
+        this.weaponStatus = weaponStatus;
+    }
+}
+
+// ExtractClass
 package refatoracoes.mover;
 
 public class ExtractClass {
-	public int health;
-	  public int damage;
-	  public int weaponStatus;
-	  public int getDamage() {
-		return damage;
 
-	  }
-	  public void attack() {
-	   // ...
-	  }
+    public int health;
+    private Weapon weapon = new Weapon();
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void attack() {
+        
+    }
 }
