f6955c9cd4cba5fb74c3985995f4665e74ee621a
Mawan Girardi Dolejal <mawan.dolejal@gmail.com>
2026-09-15 20:24:57 -0300

    Refactor SuperClass to implement Vehicle interface

--- src/main/java/refatoracoes/generalizacao/SuperClass.java
+++ src/main/java/refatoracoes/generalizacao/SuperClass.java
@@ -1,11 +1,22 @@
+// Vehicle
 package refatoracoes.generalizacao;
 
-public abstract class SuperClass {
-	private float fuel;
-	
-	public abstract void publicMethod();
-	
-	public float getFuel() {
-		return this.fuel;
-	}
+public interface Vehicle {
+    void publicMethod();
+    float getFuel();
+}
+
+
+// SuperClass
+package refatoracoes.generalizacao;
+
+public abstract class SuperClass implements Vehicle {
+
+    private float fuel;
+
+    public abstract void publicMethod();
+
+    public float getFuel() {
+        return this.fuel;
+    }
 }
