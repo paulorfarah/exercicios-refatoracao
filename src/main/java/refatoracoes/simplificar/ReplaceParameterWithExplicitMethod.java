08068d4628e52dcf107bf75f6bcb6b5e4707882d
Mawan Girardi Dolejal <mawan.dolejal@gmail.com>
2026-09-15 20:36:25 -0300

    Refactor setValue method to separate setters

--- src/main/java/refatoracoes/simplificar/ReplaceParameterWithExplicitMethod.java
+++ src/main/java/refatoracoes/simplificar/ReplaceParameterWithExplicitMethod.java
@@ -1,22 +1,21 @@
 package refatoracoes.simplificar;
 
 public class ReplaceParameterWithExplicitMethod {
-	void setValue(String name, int value, int height, int width) {
-		  if (name.equals("height")) {
-		    height = value;
-		    return;
-		  }
-		  if (name.equals("width")) {
-		    width = value;
-		    return;
-		  }
-		  Assert a = new Assert();
-		  a.shouldNeverReachHere();
-		}
-	
-	public class Assert {
-		public void shouldNeverReachHere() {
-			System.out.println("não deveria entrar aqui...");
-		}
-	}
+
+    private int height;
+    private int width;
+
+    public void setHeight(int value) {
+        this.height = value;
+    }
+
+    public void setWidth(int value) {
+        this.width = value;
+    }
+
+    public class Assert {
+        public void shouldNeverReachHere() {
+            System.out.println("não deveria entrar aqui...");
+        }
+    }
 }
