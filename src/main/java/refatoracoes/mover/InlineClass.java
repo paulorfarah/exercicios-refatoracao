4ce7b4b49224b69712f9a5bdad532d10b22ff7e8
Mawan Girardi Dolejal <mawan.dolejal@gmail.com>
2026-09-15 20:28:09 -0300

    Inline TelephoneNumber class into Person class
    Merged TelephoneNumber class into Person class and added getter and setter methods for office area code and number.

--- src/main/java/refatoracoes/mover/InlineClass.java
+++ src/main/java/refatoracoes/mover/InlineClass.java
@@ -1,22 +1,30 @@
 package refatoracoes.mover;
 
 public class InlineClass {
-	
-	public class Person {
-		String name;
-		public String getTelephoneNumber() {
-			TelephoneNumber tn = new TelephoneNumber();
-			return tn.getTelephoneNumber();
-		}
-	}
-	
-	public class TelephoneNumber {
-		private String officeAreaCode;
-		private String officeNumber;
 
-		public String getTelephoneNumber() {
-			return officeAreaCode + officeNumber;
-		}
-	}
+    public class Person {
+        String name;
+        private String officeAreaCode;
+        private String officeNumber;
+
+        public String getTelephoneNumber() {
+            return officeAreaCode + officeNumber;
+        }
+
+        public String getOfficeAreaCode() {
+            return officeAreaCode;
+        }
+
+        public void setOfficeAreaCode(String officeAreaCode) {
+            this.officeAreaCode = officeAreaCode;
+        }
+
+        public String getOfficeNumber() {
+            return officeNumber;
+        }
+
+        public void setOfficeNumber(String officeNumber) {
+            this.officeNumber = officeNumber;
+        }
+    }
 }
-
