925d24a397747d5e007d36674f39bb379bf3099c
Mawan Girardi Dolejal <mawan.dolejal@gmail.com>
2026-09-15 20:32:34 -0300

    Refactor PreserveWholeObject constructor and classes

--- src/main/java/refatoracoes/simplificar/PreserveWholeObject.java
+++ src/main/java/refatoracoes/simplificar/PreserveWholeObject.java
@@ -2,31 +2,27 @@ package refatoracoes.simplificar;
 
 public class PreserveWholeObject {
 
-	public PreserveWholeObject(DaysTempRange daysTempRange, Plan plan) {
-		int low = daysTempRange.getLow();
-		int high = daysTempRange.getHigh();
-		boolean withinPlan = plan.withinRange(low, high);
-	}
+    public PreserveWholeObject(DaysTempRange daysTempRange, Plan plan) {
+        boolean withinPlan = plan.withinRange(daysTempRange);
+    }
 
-	public class DaysTempRange {
-		int daysMin = 0;
-		int daysMax = 010;
-		
-		public int getLow() {
-			return this.daysMin;
-		}
-		
-		public int getHigh() {
-			return this.daysMax;
-		}
-	}
-	
-	public class Plan {
+    public class DaysTempRange {
+        int daysMin = 0;
+        int daysMax = 10;
 
-		public boolean withinRange(int low, int high) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-	}
+        public int getLow() {
+            return this.daysMin;
+        }
+
+        public int getHigh() {
+            return this.daysMax;
+        }
+    }
+
+    public class Plan {
+
+        public boolean withinRange(DaysTempRange range) {
+            return range.getLow() >= 0 && range.getHigh() <= 10;
+        }
+    }
 }
