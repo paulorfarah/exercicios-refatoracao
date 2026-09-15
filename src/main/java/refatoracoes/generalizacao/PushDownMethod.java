8db7d4f59bcc769fd2b453080b7a165557f4932c
Mawan Girardi Dolejal <mawan.dolejal@gmail.com>
2026-09-15 20:20:42 -0300

    Refactor PushDownMethod and SuperClass structure

--- src/main/java/refatoracoes/generalizacao/PushDownMethod.java
+++ src/main/java/refatoracoes/generalizacao/PushDownMethod.java
@@ -1,10 +1,17 @@
+// SuperClass
+package refatoracoes.generalizacao;
+
+public class SuperClass {
+	
+}
+
+
+// PushDownMethod
 package refatoracoes.generalizacao;
 
 public class PushDownMethod extends SuperClass {
 
-	public void publicMethod() {
-		System.out.println("PushDownMethod");
-		
-	}
-
+    public void publicMethod() {
+        System.out.println("PushDownMethod");
+    }
 }
