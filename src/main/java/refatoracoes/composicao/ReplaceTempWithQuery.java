45ba85807c33b172590ab5f9fa8c51fd1713c253
Mawan Girardi Dolejal <mawan.dolejal@gmail.com>
2026-09-15 20:11:27 -0300

    Refactor calculateTotal to use basePrice method

--- src/main/java/refatoracoes/composicao/ReplaceTempWithQuery.java
+++ src/main/java/refatoracoes/composicao/ReplaceTempWithQuery.java
@@ -1,13 +1,16 @@
 package refatoracoes.composicao;
 
 public class ReplaceTempWithQuery {
-	double calculateTotal(int quantity, double itemPrice) {
-		  double basePrice = quantity * itemPrice;
-		  if (basePrice > 1000) {
-		    return basePrice * 0.95;
-		  }
-		  else {
-		    return basePrice * 0.98;
-		  }
-		}
+
+    double calculateTotal(int quantity, double itemPrice) {
+        if (basePrice(quantity, itemPrice) > 1000) {
+            return basePrice(quantity, itemPrice) * 0.95;
+        } else {
+            return basePrice(quantity, itemPrice) * 0.98;
+        }
+    }
+
+    private double basePrice(int quantity, double itemPrice) {
+        return quantity * itemPrice;
+    }
 }
